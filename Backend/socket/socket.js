643e0118886f6a4901a6eb60d99a34e2b057b517
abhinavmishra26 <cseworld73@gmail.com>
2025-04-09 22:08:55 +0530

    URL update in Socket

--- Backend/socket/socket.js
+++ Backend/socket/socket.js
@@ -9,7 +9,7 @@ const server=http.createServer(app);
 
 const io=new Server(server,{
     cors:{
-        origin:["http://localhost:3000"],
+        origin:["https://real-time-chat-app-5yms.onrender.com/"],
         methods:["GET","POST"]
     },
 });
